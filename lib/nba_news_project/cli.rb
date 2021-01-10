9818385fa190bc30f2a592cb237df621cb0e531b
noah <crashlive112@gmail.com>
2021-01-10 03:09:52 -0600

    add validation check

--- lib/nba_news_project/cli.rb
+++ lib/nba_news_project/cli.rb
@@ -1,7 +1,8 @@
 class NbaNewsProject::CLI
     def call
-        puts "Welcome to NBA News"
+        puts "\nWelcome to NBA News\n"
         get_advertised_categories
+        list_category
         get_user_category
         # get_events_for(category)
         # list_events
@@ -12,10 +13,21 @@ class NbaNewsProject::CLI
         @categories = ['  -  -  Signing Rumor','  -  -  Official Signing','  -  -  Trade Rumor','  -  -  Official Trade','  -  -  Misc Rumor','  -  -  Misc Official Transaction','  -  -  Rankings','  -  -  Suspension','  -  -  Injury','  -  -  NBA Draft','  -  -  Teams','  -  -  Expansion','  -  -  CBA','  -  -  Waiver','  -  -  All-Star','  -  -  Award','  -  -  Fines','  -  -  B-Ball IQ','  -  -  Summer League','  -  -  Preseason','  -  -  Playoffs']
     end
 
-    def get_user_category
-        #list categories
-        @categories.each_with_index do |category, index| 
-        puts "#{index + 1}. #{category}"
+    def list_category
+        puts "Choose a category to see the latest news!"
+        @categories.each.with_index(1) do |category, index| 
+        puts "#{index}. #{category}"
+        end
     end
+
+    def get_user_category
+        chosen_category = gets.strip
+        binding.pry
+        # if valid_input(chosen_category.to_i, @categories)
+        # end
+    end
+
+    def valid_input(input, data) #makes sure user input is valid
+        input.to_i <= @data.length && input.to_i > 0
     end
 end
